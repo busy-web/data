872a7fce0c0f169914922431ee8cbe8bd6d7839f
Sean Stephens <seanstephens29@gmail.com>
2016-01-23 11:00:25 -0700

    added busy-data blueprint

---
+++ blueprints/busy-data/index.js
@@ -0,0 +1,22 @@
+/*jshint node:true*/
+/**
+ * @module blueprints
+ *
+ */
+module.exports = {
+	
+	//description: ''
+
+	//locals: function(options) {
+	//  // Return custom template variables here.
+	//  return {
+	//    foo: options.entity.options.foo
+	//  };
+	//}
+
+	afterInstall: function(/*options*/) 
+	{
+		// Perform extra work here.
+		this.addBowerPackageToProject('node-uuid', '~1.4.3');
+	}
+};
