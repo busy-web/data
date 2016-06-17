139af9a01ff139d4bb0538fed8a5481557d6322e
Sean Stephens <seanstephens29@gmail.com>
2016-06-17 17:37:50 -0600

    changes to authenticators

--- addon/authenticators/base.js
+++ addon/authenticators/base.js
@@ -9,12 +9,13 @@ const { getOwner } = Ember;
 
 export default Base.extend(
 {
+	authModel: '',
+
 	dataService: Ember.inject.service('busy-data'),
 
-	restore: function(data)
+	restore(data)
 	{
-		return new Ember.RSVP.Promise(function(resolve, reject)
-		{
+		return new Ember.RSVP.Promise((resolve, reject) => {
 			if(data !== undefined)
 			{
 				Ember.run(null, resolve, data);
@@ -26,97 +27,87 @@ export default Base.extend(
 		});
 	},
 
-	authenticate: function(options)
+	authenticate(options)
 	{
-		var _this = this;
-
 		Ember.assert("You must provide 'username and password' or 'token' to authenticate", (options.token || (options.username && options.password)));
 
 		if(options.username && options.password)
 		{
-			options.password = Ember.CryptoJS.SHA256(options.password).toString();
+			options.password = this.hashPassword(options.password);
 
-			return _this.ajax('member', options).then(function(member)
-			{
-				var verified = (member.data[0].email_verification_required === false || member.data[0].verified_email === true);
-
-				var auth = {
-					id: member.data[0].id,
-					public_key: member.public_key,
-					private_key: member.private_key,
-					username: options.username,
-					verifiedEmail: verified,
-					email: member.data[0].email
-				};
-
+			return this.ajax(this.get('authModel'), options).then((authData) => {
+				let authHash;
 				if(options.username && options.password)
 				{
-					auth.auth_hash = btoa(options.username + ':' + options.password);
+					authHash = btoa(options.username + ':' + options.password);
 				}
 
-				_this.onValidated();
+				let auth = this.gennerateAuthObject(authData, authHash);
+				this.onValidated();
 
 				return auth;
 			});
 		}
 		else
 		{
-			return new Ember.RSVP.Promise(function(resolve)
-			{
+			return new Ember.RSVP.Promise((resolve) => {
 				Ember.run(null, resolve, {id: options.id, public_key: options.token, verifiedEmail: true});
 			});
 		}
 	},
 
-	onValidated: function()
+	hashPassword(password)
+	{
+		return Ember.CryptoJS.SHA256(password).toString();
+	},
+
+	gennerateAuthObject(authData)
+	{
+		return authData;
+	},
+
+	onValidated()
 	{
 
 	},
 
-	onInvalidated: function()
+	onInvalidated()
 	{
 
 	},
 
-	ajaxOptions: function(url, options)
+	ajaxOptions(url, options)
 	{
+		// generate url
 		url = this.get('dataService.host') + '/' + url;
 
-		if(this.get('dataService.shouldSendVersion'))
-		{
+		// get url args
+		if(this.get('dataService.shouldSendVersion')) {
 			url = url + '?' + this.get('dataService.versionUrlParam') + '=' + this.get('dataService.version');
 		}
 
 		// set debug flag
-		if(this.get('dataService.debug'))
-		{
+		if(this.get('dataService.debug')) {
 			url = url + '&' + this.get('dataService.debugUrlParam') + '=true';
 		}
 
 		var xhr = {
 			url: url,
 			method: 'GET',
-			beforeSend: function(xhr)
-			{
-				if(options.username && options.password)
-				{
+			beforeSend(xhr) {
+				if(options.username && options.password) {
 					xhr.setRequestHeader('Authorization', 'Basic ' + btoa(options.username + ':' + options.password));
-				}
-				else if(options.token)
-				{
+				} else if(options.token) {
 					xhr.setRequestHeader('Key-Authorization', options.token);
 				}
 			}
 		};
 
-		if(options.username)
-		{
+		if(options.username) {
 			xhr.data = {
 				username: options.username
 			};
-		}
-		else if(options.id)
-		{
+		} else if(options.id) {
 			xhr.data = {
 				id: options.id
 			};
@@ -125,31 +116,23 @@ export default Base.extend(
 		return xhr;
 	},
 
-	ajax: function(url, options)
+	ajax(url, options)
 	{
 		var xhr = this.ajaxOptions(url, options);
-
-		return new Ember.RSVP.Promise(function(resolve, reject)
-		{
-			xhr.success = function(result)
-			{
-				if(typeof result === 'string')
-				{
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			xhr.success = (result) => {
+				if(typeof result === 'string') {
 					result = JSON.parse(result);
 				}
 
-				if(result.success)
-				{
+				if(result.success) {
 					Ember.run(null, resolve, result);
-				}
-				else
-				{
+				} else {
 					Ember.run(null, reject, result);
 				}
 			};
 
-			xhr.error = function(err)
-			{
+			xhr.error = (err) => {
 				Ember.run(null, reject, err);
 			};
 
@@ -157,15 +140,13 @@ export default Base.extend(
 		});
 	},
 
-	invalidate: function(session)
+	invalidate(session)
 	{
-		var auth = this;
 		var store = getOwner(this).lookup('session-store:application');
 			store.clear();
 
-		return new Ember.RSVP.Promise(function(resolve)
-		{
-			auth.onInvalidated();
+		return new Ember.RSVP.Promise((resolve) => {
+			this.onInvalidated();
 			Ember.run(null, resolve, session);
 		});
 	}
