c5c50e07f4588097fc207416602fc3c88d32eae2
Sean Stephens <seanstephens29@gmail.com>
2016-11-16 18:02:50 -0700

    feat(util): Add more join support for setting defaults in models [t3]

---
+++ addon/utils/join-all.js
@@ -0,0 +1,44 @@
+/**
+ * @module utils
+ *
+ */
+import Ember from 'ember';
+import Assert from 'busy-utils/assert';
+
+/**
+ * Model join computed property
+ *
+ */
+export default function joinAll(type, key) {
+	Assert.funcNumArgs(arguments, 2);
+	Assert.isString(type);
+	if (!Ember.isNone(key)) {
+		Assert.isString(key);
+	}
+
+  return Ember.computed(function() {
+		// get this model name
+		const modelName = this.toString().replace(/^[^:]*:([^:]*):[\s\S]*/, '$1');
+
+		// key is optional, if no key is provided
+		// the type will be used as type + id
+		if (Ember.isNone(key)) {
+			key = type.camelize() + 'Id';
+		}
+
+		// underscore the key for the api.
+		key = key.underscore();
+
+		const data = Ember.ArrayProxy.extend({
+			content: null,
+			method: 'joinAll',
+			modelType: type,
+			join: modelName,
+			joinOn: key,
+			_isJoin: true
+		});
+
+		// return the join object with all the data
+		return data.create([]);
+	});
+}
--- addon/utils/manager.js
+++ addon/utils/manager.js
@@ -5,6 +5,7 @@
 import Ember from 'ember';
 import RequestHandler from 'busy-data/utils/request-handler';
 import Helper from 'busy-data/utils/helpers';
+import Assert from 'busy-utils/assert';
 
 const {getModelProperty, setModelProperty, generateModelPath, generateId, mergeObject} = Helper;
 
@@ -45,84 +46,118 @@ export default Ember.Object.extend(
 	__collection: null,
 	__storedOperations: null,
 
-	query: function(modelType, query, alias)
-	{
-		query = query || {};
+	__lastOperation: null,
+	__nestedOperation: null,
 
+	query(modelType, query={}, alias=null) {
 		this.addOperation('query', modelType, {query: query}, alias);
-
 		return this;
 	},
 
-	queryRecord: function(modelType, query, alias)
-	{
-		query = query || {};
-
+	queryRecord(modelType, query={}, alias=null) {
 		this.addOperation('queryRecord', modelType, {query: query}, alias);
-
 		return this;
 	},
 
-	findAll: function(modelType, query, alias)
-	{
-		query = query || {};
-
+	findAll(modelType, query={}, alias=null) {
 		this.addOperation('findAll', modelType, {query: query}, alias);
-
 		return this;
 	},
 
-	findWhereIn: function(modelType, key, id, query, alias)
-	{
-		query = query || {};
-
+	findWhereIn(modelType, key, id, query={}, alias=null) {
 		this.addOperation('findWhereIn', modelType, {key: key, id: id, query: query}, alias);
-
 		return this;
 	},
 
-	findRecord: function(modelType, id, alias)
-	{
+	findRecord(modelType, id, alias) {
 		this.addOperation('findRecord', modelType, {id: id}, alias);
-
 		return this;
 	},
 
-	outerJoin: function(modelType, join, joinOn, query, alias)
-	{
+	outerJoin(modelType, join, joinOn, query, alias) {
 		this.addOperation('outerJoin', modelType, {join: join, joinOn: joinOn, query: query}, alias);
-
 		return this;
 	},
 
-	outerJoinAll: function(modelType, join, joinOn, query, alias)
-	{
+	outerJoinAll(modelType, join, joinOn, query, alias) {
 		this.addOperation('outerJoinAll', modelType, {join: join, joinOn: joinOn, query: query}, alias);
-
 		return this;
 	},
 
-	join: function(modelType, join, joinOn, query, alias)
-	{
+	join(modelType, join, joinOn, query, alias) {
 		this.addOperation('join', modelType, {join: join, joinOn: joinOn, query: query}, alias);
-
 		return this;
 	},
 
-	joinAll: function(modelType, join, joinOn, query, alias)
-	{
+	joinAll(modelType, join, joinOn, query, alias) {
 		this.addOperation('joinAll', modelType, {join: join, joinOn: joinOn, query: query}, alias);
+		return this;
+	},
+
+	callJoinFromModel(lastOperation, key) {
+		Assert.funcNumArgs(arguments, 2, true);
+		Assert.test('You must have a record type to call a `get` join model for that record', !Ember.isNone(lastOperation));
+
+		// get the join data from the last operations model
+		const modelName = lastOperation.get('modelType');
+		const model = this.store.createRecord(modelName, {});
+		const val = model.get(key);
+
+		// throw error if not a valid join object
+		Assert.test(`get did not find a valid join param at key [${key}]`, !Ember.isNone(val) && typeof val === 'object' && val._isJoin === true);
+
+		this[val.method].call(this, val.modelType, val.join, val.joinOn, {}, key);
+	},
+
+	get(key) {
+		Assert.funcNumArgs(arguments, 1, true);
+
+		// get the last operation to join onto
+		let lastOperation = this.__lastOperation;
+
+		// call method to add join
+		this.callJoinFromModel(lastOperation, key);
+
+		this.__lastOperation = lastOperation;
+		return this;
+	},
+
+	beginGet(key) {
+		Assert.funcNumArgs(arguments, 1, true);
+
+		// get the last operation to join onto
+		let lastOperation = this.__lastOperation;
+
+		// call method to add join
+		this.callJoinFromModel(lastOperation, key);
+
+		// if no nested create new nested array map
+		if (Ember.isNone(this.__nestedOperation)) {
+			this.__nestedOperation = [];
+		}
+
+		// push the last operation to the nested map
+		this.__nestedOperation.push(lastOperation);
 
 		return this;
 	},
 
-	collection: function(type)
-	{
-		var owner = Ember.getOwner(this);
+	endGet() {
+		// pop the lastoperation off the nested map
+		this.__lastOperation = this.__nestedOperation.pop();
 
-		var Collection = owner._lookupFactory('collection:' + Ember.String.dasherize(type));
+		// if the array is empty set it back to null
+		if (Ember.isEmpty(this.__nestedOperation)) {
+			this.__nestedOperation = null;
+		}
 
-		var collection = Collection.create({
+		return this;
+	},
+
+	collection(type) {
+		const owner = Ember.getOwner(this);
+		const Collection = owner._lookupFactory('collection:' + Ember.String.dasherize(type));
+		const collection = Collection.create({
 			store: this.store,
 			manager: this,
 			getter: this,
@@ -134,13 +169,10 @@ export default Ember.Object.extend(
 		return this;
 	},
 
-	polymorph: function(type)
-	{
-		var owner = Ember.getOwner(this);
-
-		var Polymorph = owner._lookupFactory('polymorph:' + Ember.String.dasherize(type));
-
-		var polymorph = Polymorph.create({
+	polymorph(type) {
+		const owner = Ember.getOwner(this);
+		const Polymorph = owner._lookupFactory('polymorph:' + Ember.String.dasherize(type));
+		const polymorph = Polymorph.create({
 			store: this.store,
 			manager: this,
 			getter: this,
@@ -153,42 +185,23 @@ export default Ember.Object.extend(
 		return this;
 	},
 
-	clearPolymorph: function()
-	{
+	clearPolymorph() {
 		this.__polymorph = null;
-
 		return this;
 	},
 
-	addOperation: function(type, modelType, params, alias)
-	{
-		var id = generateId();
+	addOperation(operationType, modelType, params, alias) {
+		const id = generateId();
+		const operation = Ember.Object.create({ id, alias, params, operationType, modelType, polymorph: this.__polymorph });
 
-		this.operations.pushObject(Ember.Object.create({
-			id: id,
-			operationType: type,
-			modelType: modelType,
-			alias: alias,
-			params: params,
-			polymorph: this.__polymorph
-		}));
-
-		this.__storedOperations.pushObject(Ember.Object.create({
-			id: id,
-			operationType: type,
-			modelType: modelType,
-			alias: alias,
-			params: params,
-			polymorph: this.__polymorph
-		}));
+		this.operations.pushObject(operation);
+		this.__storedOperations.pushObject(Ember.Object.create({ id, alias, params, operationType, modelType, polymorph: this.__polymorph }));
+		this.__lastOperation = operation;
 	},
 
-	reloadOperations: function()
-	{
-		var _this = this;
-		this.__storedOperations.forEach(function(item)
-		{
-			_this.operations.pushObject(Ember.Object.create({
+	reloadOperations() {
+		this.__storedOperations.forEach(item => {
+			this.operations.pushObject(Ember.Object.create({
 				id: item.get('id'),
 				operationType: item.get('operationType'),
 				modelType: item.get('modelType'),
@@ -199,59 +212,46 @@ export default Ember.Object.extend(
 		});
 	},
 
-	update: function()
-	{
+	update() {
 		this.reloadOperations();
-
 		return this.fetch();
 	},
 
-	applyPolymorphs: function(data)
-	{
-		var operations = this.get('__storedOperations');
+	applyPolymorphs(data) {
+		const operations = Ember.get(this, '__storedOperations');
+		const dataMap = {};
+		operations.forEach(item => {
+			const type = item.get('modelType');
+			const parentType = item.get('query.join');
+			const path = generateModelPath(parentType, type);
 
-		var dataMap = {};
-
-		operations.forEach(function(item)
-		{
-			var type = item.get('modelType');
-			var parentType = item.get('query.join');
-			var path = generateModelPath(parentType, type);
-
-			var polymorph = item.get('polymorph');
-			if(!Ember.isNone(polymorph))
-			{
-				var model = getModelProperty(data, path);
+			const polymorph = item.get('polymorph');
+			if (!Ember.isNone(polymorph)) {
+				const model = getModelProperty(data, path);
 				setModelProperty(data, path, null);
 
-				var polyName = generateModelPath(parentType, polymorph.get('_name'));
-				if(getModelProperty(dataMap, polyName) === undefined)
-				{
+				const polyName = generateModelPath(parentType, polymorph.get('_name'));
+				if (getModelProperty(dataMap, polyName) === undefined) {
 					setModelProperty(dataMap, polyName, polymorph);
 				}
-
 				setModelProperty(dataMap, generateModelPath(polyName, type), model);
 			}
 		});
 
-		for(let i in data)
-		{
-			if(data.hasOwnProperty(i))
-			{
-				if(data[i] === null)
-				{
+		for (let i in data) {
+			if (data.hasOwnProperty(i)) {
+				if (data[i] === null) {
 					delete data[i];
 				}
 			}
 		}
-
 		return buildDataObject(data, dataMap);
 	},
 
 	fetch() {
 		const args = arguments;
 		const requester = RequestHandler.create({store: this.store, finishedList: Ember.A()});
-		const operations = this.get('operations');
+		const operations = Ember.get(this, 'operations');
 		if (operations.length === 0) {
 			return this.__collection.model.apply(this.__collection, args);
 		} else {
