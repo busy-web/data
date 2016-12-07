4aa757c11ae02d4546591cba59b5fbca59202e3b
Sean Stephens <seanstephens29@gmail.com>
2016-12-07 16:34:38 -0700

    fix(adapter) compiled code missing || {} syntax

--- addon/adapters/json.js
+++ addon/adapters/json.js
@@ -53,7 +53,11 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 			}
 		}
 
-		let data = hash.data || {};
+		let data = hash.data;
+		if (Ember.isNone(data)) {
+			data = {};
+		}
+
 		let isString = false;
 		if (typeof data === 'string') {
 			isString = true;
