34dce8d252b758c4846db6573301b0f3bc1200a3
Sean Stephens <seanstephens29@gmail.com>
2017-02-23 18:24:07 -0700

    fix(auth): added better authentication from token

--- addon/authenticators/base.js
+++ addon/authenticators/base.js
@@ -8,87 +8,70 @@ import { Hash } from 'busy-utils';
 
 const { getOwner } = Ember;
 
-export default Base.extend(
-{
+export default Base.extend({
 	authModel: '',
-
 	dataService: Ember.inject.service('busy-data'),
 
-	restore(data)
-	{
+	restore(data) {
 		return new Ember.RSVP.Promise((resolve, reject) => {
-			if(data !== undefined)
-			{
+			if (data !== undefined) {
 				Ember.run(null, resolve, data);
-			}
-			else
-			{
+			} else {
 				Ember.run(null, reject, data);
 			}
 		});
 	},
 
-	authenticate(options)
-	{
+	authenticate(options) {
 		Ember.assert("You must provide 'username and password' or 'token' to authenticate", (options.token || (options.username && options.password)));
 
 		options.success = typeof options.success === 'function' ? options.success : function(){};
 		options.error = typeof options.error === 'function' ? options.error : function(){};
 
-		if(options.username && options.password)
-		{
+		if (options.username && options.password) {
 			options.password = this.hashPassword(options.password);
-
-			return new Ember.RSVP.Promise((resolve, reject) => {
-				//success function
-				const success = (authData) => {
-
-					// create a secure username and password hash to pass around.
-					let authHash;
-					if(options.username && options.password) {
-						authHash = btoa(options.username + ':' + options.password);
-					}
-
-					// pass the secure username password and the authData from the api call
-					const auth = this.gennerateAuthObject(authData, authHash);
-
-					// validate the results
-					const inValid = this.isInvalid(auth);
-
-					if(!inValid) {
-						this.onValidated();
-						options.success(auth);
-						Ember.run(null, resolve, auth);
-					} else {
-						options.error({status: inValid});
-						Ember.run(null, reject, {status: 401, statusText: "Unauthorized"});
-					}
-				};
-
-				// error function
-				const error = (err) => {
-					options.error(err);
-					Ember.run(null, reject, err);
-				};
-
-				this.ajax(this.get('authModel'), options, success, error);
-			});
 		}
-		else
-		{
-			return new Ember.RSVP.Promise((resolve) => {
-				Ember.run(null, resolve, {id: options.id, public_key: options.token, verifiedEmail: true});
-			});
-		}
+
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			//success function
+			const success = (authData) => {
+				// create a secure username and password hash to pass around.
+				let authHash;
+				if(options.username && options.password) {
+					authHash = window.btoa(options.username + ':' + options.password);
+				}
+
+				// pass the secure username password and the authData from the api call
+				const auth = this.gennerateAuthObject(authData, authHash, options.token);
+
+				// validate the results
+				const inValid = this.isInvalid(auth);
+
+				if(!inValid) {
+					this.onValidated();
+					options.success(auth);
+					Ember.run(null, resolve, auth);
+				} else {
+					options.error({status: inValid});
+					Ember.run(null, reject, {status: 401, statusText: "Unauthorized"});
+				}
+			};
+
+			// error function
+			const error = (err) => {
+				options.error(err);
+				Ember.run(null, reject, err);
+			};
+
+			this.ajax(this.get('authModel'), options, success, error);
+		});
 	},
 
-	hashPassword(password)
-	{
+	hashPassword(password) {
 		return Hash.sha256(password).toString();
 	},
 
-	gennerateAuthObject(authData)
-	{
+	gennerateAuthObject(authData) {
 		return authData;
 	},
 
@@ -99,25 +82,22 @@ export default Base.extend(
 	 * @param {object} auth
 	 * @return {mixed} true if valid.
 	 */
-	isInvalid(/*auth*/)
-	{
+	isInvalid(/*auth*/) {
 		return;
 	},
 
-	onValidated()
-	{
+	onValidated() {},
 
-	},
+	onInvalidated() {},
 
-	onInvalidated()
-	{
-
-	},
-
-	ajaxOptions(url, options)
-	{
+	ajaxOptions(url, options) {
 		// generate url
 		url = this.get('dataService.host') + '/' + url;
+
+		if (options.id) {
+			url += '/' + options.id;
+			delete options.id;
+		}
 
 		// get url args
 		if(this.get('dataService.shouldSendVersion')) {
@@ -134,7 +114,7 @@ export default Base.extend(
 			method: 'GET',
 			beforeSend(xhr) {
 				if(options.username && options.password) {
-					xhr.setRequestHeader('Authorization', 'Basic ' + btoa(options.username + ':' + options.password));
+					xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(options.username + ':' + options.password));
 				} else if(options.token) {
 					xhr.setRequestHeader('Key-Authorization', options.token);
 				}
@@ -142,26 +122,16 @@ export default Base.extend(
 		};
 
 		if(options.username) {
-			xhr.data = {
-				username: options.username
-			};
-		} else if(options.id) {
-			xhr.data = {
-				id: options.id
-			};
+			xhr.data = { username: options.username };
 		}
-
 		return xhr;
 	},
 
-	ajax(url, options, onSuccess, onError)
-	{
-		var xhr = this.ajaxOptions(url, options);
-		xhr.success = (result) => {
-			if(typeof result === 'string') {
-				result = JSON.parse(result);
-			}
+	ajax(url, options, onSuccess, onError) {
+		const xhr = this.ajaxOptions(url, options);
 
+		xhr.success = (result) => {
+			if(typeof result === 'string') { result = JSON.parse(result); }
 			if(result.success) {
 				onSuccess(result);
 			} else {
@@ -176,11 +146,9 @@ export default Base.extend(
 		Ember.$.ajax(xhr);
 	},
 
-	invalidate(session)
-	{
-		var store = getOwner(this).lookup('session-store:application');
-			store.clear();
-
+	invalidate(session) {
+		const store = getOwner(this).lookup('session-store:application');
+		store.clear();
 		return new Ember.RSVP.Promise((resolve) => {
 			this.onInvalidated();
 			Ember.run(null, resolve, session);
