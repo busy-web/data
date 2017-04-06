0639144985c46c4fde020c5478c02c3d5e850690
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 17:24:43 -0600

    chore(blueprints): updated included packages

--- blueprints/busy-data/index.js
+++ blueprints/busy-data/index.js
@@ -1,9 +1,13 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
 
 	normalizeEntityName() {},
 
 	afterInstall(/*options*/)  {
-		return this.addAddonToProject('ember-simple-auth@1.1.0');
+		return this.addAddonToProject('ember-data', '2.12.1').then(() => {
+			return this.addAddonToProject('ember-simple-auth', '1.2.1').then(() => {
+				return this.addAddonToProject('busy-utils', '2.4.0');
+			});
+		});
 	}
 };
