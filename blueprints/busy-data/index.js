f95fa4eae494427a0ae70156dae3cd51a3c41eb0
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 18:10:14 -0600

    updates

--- blueprints/busy-data/index.js
+++ blueprints/busy-data/index.js
@@ -6,7 +6,7 @@ module.exports = {
 	afterInstall(/*options*/)  {
 		return this.addAddonToProject('ember-data', '2.12.1').then(() => {
 			return this.addAddonToProject('ember-simple-auth', '1.2.1').then(() => {
-				return this.addAddonToProject('busy-utils', '2.4.0');
+				return this.addAddonToProject('busy-utils', '2.4.1');
 			});
 		});
 	}
