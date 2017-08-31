9e6855d4bb919e2ba27840b68f808ef997d4e52b
Sean Stephens <seanstephens29@gmail.com>
2017-08-31 16:38:20 -0600

    more additions

---
+++ addon/mixins/store-finders.js
@@ -0,0 +1,130 @@
+/**
+ * @module store
+ *
+ */
+import Ember from 'ember';
+
+/***/
+const MAX_PAGE_SIZE = 100;
+const { isNone, isEmpty, get, set, merge, getWithDefault } = Ember;
+
+function isObject(value) {
+	return !isNone(value) && typeof value === 'object' && value.length === undefined;
+}
+
+function nextParams(model, query, lastQuery) {
+	let isJsonApi = false;
+	let next = get(model, 'meta.next');
+	if (isNone(next)) {
+		isJsonApi = true;
+		next = get(model, 'links.next');
+	}
+
+	if (!isEmpty(next)) {
+		if (isJsonApi) {
+			let [ , params ] = next.split('?');
+			params = params.split('&');
+			params.forEach(item => {
+				let [ key, value ] = item.split('=');
+				if (/\[/.test(key)) {
+					let [ key2, key3 ] = key.split('[');
+					key3 = key3.replace(/\]$/, '');
+					key = key2;
+					if (/\d+/.test(key3)) {
+						value = [value];
+					} else {
+						value = {[key3]: value};
+					}
+				}
+				set(query, key, value);
+			});
+		} else {
+			lastQuery.page = lastQuery.page + 1;
+			merge(query, lastQuery);
+		}
+		return true;
+	}
+	return false;
+}
+
+
+function _findWhereIn(store, modelType, queryList, query) {
+	if (queryList.length === 0) {
+		return Ember.RSVP.resolve([]);
+	}
+
+	// get next query params
+	const params = queryList.shift();
+
+	// merge query params with params
+	merge(params, query);
+
+	// find all models
+	return store.findAll(modelType, params).then((model) => {
+		return _findWhereIn(store, modelType, queryList, query).then((moreModels) => {
+			if (isObject(moreModels) && !isEmpty(get(moreModels, 'content'))) {
+				return model.pushObjects(get(moreModels, 'content'));
+			}
+			return model;
+		});
+	});
+}
+
+/**
+ * `StoreFinders`
+ *
+ * Mixin that adds extra find methods to the store service
+ */
+export default Ember.Mixin.create({
+	_maxPageSize: MAX_PAGE_SIZE,
+
+	findAll(modelType, _query={}) {
+		// copy query object
+		const query = merge({}, _query);
+
+		if (isNone(get(query, 'limit'))) {
+			set(query, 'page_size', getWithDefault(query, 'page_size', this._maxPageSize));
+			set(query, 'page', getWithDefault(query, 'page', 1));
+		}
+
+		// make query request
+		return this.query(modelType, query).then(models => {
+			let nextQuery = {};
+			if (nextParams(models, nextQuery, query)) {
+				return this.findAll(modelType, nextQuery).then(moreModels => {
+					if (isObject(moreModels) && !isEmpty(get(moreModels, 'content'))) {
+						models.pushObjects(get(moreModels, 'content'));
+					}
+					return models;
+				});
+			} else {
+				return models;
+			}
+		});
+	},
+
+	findByIds(modelType, ids, query={}) {
+		return this.findWhereIn(modelType, 'id', ids, query);
+	},
+
+	findWhereIn(modelType, key, values, query={}) {
+		Ember.assert('modelType must be of type string in store.findWhereIn()', typeof modelType === 'string');
+		Ember.assert('key must be of type string in store.findWhereIn()', typeof key === 'string');
+		Ember.assert('values must be an array of strings in store.findWhereIn()', Ember.typeOf(values) === 'array');
+		Ember.assert('query must be an object in store.findWhereIn()', typeof query === 'object');
+
+		const _values = values.slice(0);
+
+		const queryList = [];
+		while(_values.length) {
+			const __values = _values.splice(0, this._maxPageSize);
+			if (/^!/.test(key)) {
+				queryList.push({ _not_in: { [key]: __values } });
+			} else {
+				queryList.push({ _in: { [key]: __values } });
+			}
+		}
+
+		return _findWhereIn(this, modelType, queryList, query);
+	}
+});
