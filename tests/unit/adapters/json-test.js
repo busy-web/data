126b88617924bdff83b5bba8aecf9cbb5d0e8927
Sean Stephens <seanstephens29@gmail.com>
2017-10-30 16:52:07 -0600

    fixed tests

--- tests/unit/adapters/json-test.js
+++ tests/unit/adapters/json-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('adapter:json', 'Unit | Adapter | json', {
   // Specify the other units that are required for this test.
-	needs: ['service:session']
+	needs: []
 });
 
 // Replace this with your real tests.
